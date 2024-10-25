4eb45474298c8ea1eae9858179143582c4fd34fb
Marcap00 <cappellatomarco@yahoo.com>
2024-10-25 18:52:10 +0200

    added methods to format dates

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function getFormattedDepartureDate()
+    {
+        return date('D F Y', strtotime($this->departure_date));
+    }
+
+    public function getFormattedArrivalDate()
+    {
+        return date('D F Y', strtotime($this->arrival_date));
+    }
 }
