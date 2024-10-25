4b05da1c8eb3ef674af4a171150bf1e9fd340b19
Marcap00 <cappellatomarco@yahoo.com>
2024-10-25 17:41:51 +0200

    added train controller

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function home()
+    {
+        // Recupero i dati dei treni dal database
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
+    }
+}
