d39d1d16769de24b5aee1cae2f41c8e128d6587c
Marcap00 <cappellatomarco@yahoo.com>
2024-10-25 21:57:37 +0200

    created methods for format time and try to use carbon

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Train extends Model
 {
@@ -12,10 +14,22 @@ class Train extends Model
     public function getFormattedDepartureDate()
     {
         return date('D F Y', strtotime($this->departure_date));
+        /* return Carbon::createFromFormat('Y-m-d', $this->departure_date)->toFormattedDateString(); */
     }
 
     public function getFormattedArrivalDate()
     {
         return date('D F Y', strtotime($this->arrival_date));
+        /* return Carbon::createFromFormat('D F Y', $this->arrival_date)->toFormattedDateString(); */
+    }
+
+    public function getFormattedDepartureTime()
+    {
+        return date('H:i', strtotime($this->departure_time));
+    }
+
+    public function getFormattedArrivalTime()
+    {
+        return date('H:i', strtotime($this->arrival_time));
     }
 }
