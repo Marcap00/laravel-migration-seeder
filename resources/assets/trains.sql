c2a235394f8c88aff969cacd8fc5f8be6579d3e9
Marcap00 <cappellatomarco@yahoo.com>
2024-10-25 18:09:17 +0200

    Added file sql that popolated database

---
+++ resources/assets/trains.sql
@@ -0,0 +1,24 @@
+INSERT INTO trains (train_code, company, departure_station, arrival_station, departure_date, arrival_date, departure_time, arrival_time, number_of_carriages, on_time, deleted)
+VALUES 
+('IC001', 'Trenitalia', 'Roma Termini', 'Milano Centrale', '2024-10-24', '2024-10-24', '08:30:00', '12:15:00', 10, TRUE, FALSE),
+('FR555', 'Italo', 'Napoli Centrale', 'Torino Porta Nuova', '2024-10-25', '2024-10-25', '09:45:00', '14:30:00', 8, TRUE, FALSE),
+('RE002', 'Trenitalia', 'Bologna Centrale', 'Firenze SMN', '2024-10-26', '2024-10-26', '10:15:00', '11:00:00', 5, TRUE, FALSE),
+('IC056', 'Trenitalia', 'Venezia Santa Lucia', 'Verona Porta Nuova', '2024-10-24', '2024-10-24', '11:45:00', '13:30:00', 7, FALSE, FALSE),
+('FR660', 'Italo', 'Milano Centrale', 'Roma Termini', '2024-10-25', '2024-10-25', '13:00:00', '16:45:00', 9, TRUE, FALSE),
+('RE099', 'Trenord', 'Milano Centrale', 'Bergamo', '2024-10-25', '2024-10-25', '07:30:00', '08:20:00', 4, TRUE, FALSE),
+('IC120', 'Trenitalia', 'Genova Piazza Principe', 'Pisa Centrale', '2024-10-24', '2024-10-24', '06:30:00', '08:50:00', 6, FALSE, FALSE),
+('RE101', 'Trenord', 'Lecco', 'Milano Centrale', '2024-10-25', '2024-10-25', '15:00:00', '16:00:00', 3, TRUE, FALSE),
+('FR330', 'Italo', 'Roma Tiburtina', 'Venezia Mestre', '2024-10-26', '2024-10-26', '17:45:00', '21:15:00', 8, FALSE, FALSE),
+('IC145', 'Trenitalia', 'Salerno', 'Napoli Centrale', '2024-10-24', '2024-10-24', '14:30:00', '15:10:00', 5, TRUE, FALSE),
+('IC205', 'Trenitalia', 'Roma Termini', 'Firenze SMN', '2024-10-25', '2024-10-25', '18:00:00', '19:30:00', 9, TRUE, FALSE),
+('FR777', 'Italo', 'Milano Centrale', 'Napoli Centrale', '2024-10-24', '2024-10-24', '19:15:00', '23:00:00', 11, FALSE, FALSE),
+('RE303', 'Trenord', 'Torino Porta Susa', 'Milano Centrale', '2024-10-25', '2024-10-25', '05:45:00', '07:05:00', 6, TRUE, FALSE),
+('IC308', 'Trenitalia', 'Verona Porta Nuova', 'Bologna Centrale', '2024-10-26', '2024-10-26', '08:20:00', '10:00:00', 8, FALSE, FALSE),
+('FR505', 'Italo', 'Venezia Santa Lucia', 'Roma Termini', '2024-10-24', '2024-10-24', '15:00:00', '18:45:00', 10, TRUE, FALSE),
+('RE404', 'Trenitalia', 'Napoli Centrale', 'Salerno', '2024-10-25', '2024-10-25', '07:30:00', '08:10:00', 4, TRUE, FALSE),
+('IC700', 'Trenitalia', 'Genova Piazza Principe', 'Milano Centrale', '2024-10-25', '2024-10-25', '14:30:00', '16:10:00', 7, FALSE, FALSE),
+('RE505', 'Trenord', 'Pavia', 'Milano Centrale', '2024-10-24', '2024-10-24', '12:20:00', '13:00:00', 3, TRUE, FALSE),
+('FR900', 'Italo', 'Torino Porta Nuova', 'Firenze SMN', '2024-10-26', '2024-10-26', '09:00:00', '12:30:00', 9, TRUE, FALSE),
+('IC910', 'Trenitalia', 'Bologna Centrale', 'Venezia Mestre', '2024-10-24', '2024-10-24', '17:00:00', '18:30:00', 6, TRUE, FALSE);
+
+
