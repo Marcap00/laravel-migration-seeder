15c50f864122cd704095d40ed05621877b6dee57
Marcap00 <cappellatomarco@yahoo.com>
2024-10-25 17:43:11 +0200

    use TrainController for routing home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TrainController;
+use App\Models\Train;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
+Route::get('/', [TrainController::class, 'home'])->name('home');
