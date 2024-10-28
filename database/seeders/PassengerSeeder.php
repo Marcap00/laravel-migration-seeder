bcb8331be32de37a837015f1bad1de7528cede11
Marcap00 <cappellatomarco@yahoo.com>
2024-10-28 12:36:59 +0100

    -added PassengerSeeder to DatabaseSeeder - populated column passengers table

--- database/seeders/PassengerSeeder.php
+++ database/seeders/PassengerSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Passenger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PassengerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newPassenger = new Passenger();
+            $newPassenger->name = $faker->firstName();
+            $newPassenger->surname = $faker->lastName();
+            $newPassenger->number_carriage = $faker->numberBetween(1, 20);
+            $newPassenger->number_seat = $faker->numberBetween(1, 100);
+            $newPassenger->save();
+        }
     }
 }
